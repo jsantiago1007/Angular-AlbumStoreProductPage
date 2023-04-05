14c6a3cf3ee86ea15cad1ed3aa990d9ef704b504
jsantiago1007 <santiagojimwell10@gmail.com>
2023-04-05 11:15:27 +0800

    6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,13 @@ import { ProductService } from './product.service';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {path:'products', component: ProductListComponent},
+  {path:'product/:id', component: ProductPageComponent},
+  {path:'', redirectTo: 'products', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
@@ -19,7 +26,8 @@ import { ProductListComponent } from './product-list/product-list.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     ProductService
